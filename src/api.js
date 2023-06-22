9a8b14cd9d3db5e94a48d7a1302fc0df76df00fa
deril1709 <punktator17@gmail.com>
2023-06-22 12:49:54 +0800

    add api

--- src/api.js
+++ src/api.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+const getMovielist = async () => {
+  const movie = await axios.get("");
+  return;
+};
+
+export const searchMovie = async (q) => {
+  const search = await axios.get(q);
+  return;
+};
