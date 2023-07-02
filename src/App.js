287f79a65b3d15bc1a0a466fee53e19e4b7e7292
deril1709 <punktator17@gmail.com>
2023-07-02 17:18:12 +0700

    update design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getMovielist, searchMovie } from "./api";
 
+
 const App = () => {
   const [popularMovies, setPopularMovies] = useState ([])
 
